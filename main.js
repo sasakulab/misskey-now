03303983f0a9b69b089557d7146c730ba4c1d265
sasakulari <sasakura.ayato.syosetu@gmail.com>
2023-02-12 01:19:15 +0900

    Lint: main.js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 // 初期化
 var manifestData = chrome.runtime.getManifest();
-var saveSettings = {}
-ConvertVariableCheck()
+var saveSettings = {};
+ConvertVariableCheck();
 
 // 移行処理（移行チェック）
 function ConvertVariableCheck() {
     chrome.storage.local.get(['version', 'instance', 'key']).then((results) => {
-        version = results.version,
-        instance = results.instance,
-        key = results.key
+        (version = results.version),
+            (instance = results.instance),
+            (key = results.key);
 
         if (version == '0.2.0') {
             console.log('Misskey Now: Settings have Already Updated!');
-        } else if (typeof instance === 'undefined' && typeof key === 'undefined') {
+        } else if (
+            typeof instance === 'undefined' &&
+            typeof key === 'undefined'
+        ) {
             console.log(
                 'Misskey Now: Thank you for installing Misskey Now! Initialize Configuration.'
             );
@@ -42,13 +45,13 @@ function ConvertVariableCheck() {
                 console.log('Misskey Now: Saving Settings (Replaced).');
             });
         }
-    })
+    });
 }
 
 // 動作変数設定
 
 function getUrl() {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const url = tabs[0].url;
         const title = tabs[0].title;
         popup_title.value = title;
@@ -79,10 +82,7 @@ function generateNote() {
     const range = popup_range.value;
     const hash = popup_hash.value;
     const note = popup_note.value;
-    const host =
-        'https://' +
-        settings_host.value +
-        '/api/notes/create';
+    const host = 'https://' + settings_host.value + '/api/notes/create';
     const apiKey = settings_api_key.value;
     strings = note + '\n\n' + '『' + title + '』 - ' + url + ' ' + hash;
     console.log(strings);
@@ -123,7 +123,7 @@ function generateNote() {
 function saveSetting() {
     profileName = settings_profile_name.value;
     if (popup_profile.value !== 'new') {
-        delete saveSettings[[popup_profile.value]]
+        delete saveSettings[[popup_profile.value]];
     }
     instance = settings_host.value;
     key = settings_api_key.value;
@@ -133,8 +133,8 @@ function saveSetting() {
     };
     settings = {
         version: manifestData.version,
-        profiles: saveSettings
-    }
+        profiles: saveSettings,
+    };
     chrome.storage.local.set(settings, function () {
         console.log('Misskey-Now: Stored New Settings.');
         const prevText = save_settings.textContent;
@@ -148,13 +148,13 @@ function saveSetting() {
             save_settings.textContent = prevText;
             save_settings.className = prevClass;
         }, 1500);
-        displayProfiles()
+        displayProfiles();
     });
 }
 
 function changeProfile() {
-    var selected = popup_profile.value
-    if(selected !== 'new'){
+    var selected = popup_profile.value;
+    if (selected !== 'new') {
         settings_profile_name.value = selected;
         settings_host.value = saveSettings[[selected]].instance;
         settings_api_key.value = saveSettings[[selected]].key;
@@ -166,28 +166,28 @@ function changeProfile() {
 }
 
 function displayProfiles() {
-    removeChildren(popup_profile)
+    removeChildren(popup_profile);
     const option = document.createElement('option');
-    option.textContent = "New Profile"
-    option.value = "new"
-    popup_profile.appendChild(option)
-    Object.keys(saveSettings).forEach(profile => {
+    option.textContent = 'New Profile';
+    option.value = 'new';
+    popup_profile.appendChild(option);
+    Object.keys(saveSettings).forEach((profile) => {
         const option = document.createElement('option');
         option.textContent = profile;
         option.value = profile;
         popup_profile.appendChild(option);
-    })
+    });
 }
 
 function removeProfile() {
     if (popup_profile.value !== 'new') {
-        console.log('debug')
-        delete saveSettings[[popup_profile.value]]
+        console.log('debug');
+        delete saveSettings[[popup_profile.value]];
     }
     settings = {
         version: manifestData.version,
-        profiles: saveSettings
-    }
+        profiles: saveSettings,
+    };
     chrome.storage.local.set(settings, function () {
         console.log('Misskey-Now: Stored New Settings.');
         settings_profile_name.value = '';
@@ -209,10 +209,12 @@ function handleCtrlEnter(e) {
         document.querySelector('.btn-send')?.click();
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     getUrl();
     document.querySelector('.btn-send').addEventListener('click', generateNote);
     document.querySelector('.btn-save').addEventListener('click', saveSetting);
-    document.querySelector('.btn-delete').addEventListener('click', removeProfile);
+    document
+        .querySelector('.btn-delete')
+        .addEventListener('click', removeProfile);
     popup_profile.addEventListener('change', changeProfile);
 });
