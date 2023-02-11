320cae0f046728a3b3f282bbaf01251aea4b54b9
sasakulari <sasakura.ayato.syosetu@gmail.com>
2023-02-11 09:17:21 +0900

    Lint: Lint

--- main.js
+++ main.js
@@ -1,109 +1,129 @@
 let manifestData = chrome.runtime.getManifest();
+var saveSettings = {};
 
-chrome.storage.local.get(["version", "profiles"]).then((results) => {
-    if (typeof results.version === 'undefined' | typeof results.profiles === 'undefined' ){ // 初回起動
+chrome.storage.local.get(['version', 'profiles']).then((results) => {
+    if (
+        (typeof results.version === 'undefined') |
+        (typeof results.profiles === 'undefined')
+    ) {
+        // 初回起動
+        console.log(
+            'Misskey Now: Thank you for installing! Initialize Settings.'
+        );
         init = {
             version: manifestData.version,
-            profiles: []
+            profiles: {},
         };
-        chrome.storage.local.set(init, function() {
-            console.log('Misskey-Now: Initialize Settings (First Startup).');
+        chrome.storage.local.set(init, function () {
+            console.log('Misskey-Now: Now Saving Settings (First Startup).');
         });
-    } else if (typeof results.version == 'undefined' | typeof results.instance !== 'undefined'){ // バージョン情報がない -> データ構造更新
-        document.getElementById("settings_host").value = results.instance;
-        document.getElementById("settings_api_key").value = results.key;
-    } else { // version も登録され、profiles も保存済み -> 通常起動
-
-    };
-    console.log(results)
+    } else if (
+        (typeof results.version == 'undefined') |
+        (typeof results.instance !== 'undefined')
+    ) {
+        // バージョン情報がない -> データ構造更新
+        console.log('Misskey Now: Thank you for Updating! Update Settings.');
+        document.getElementById('settings_host').value = results.instance;
+        document.getElementById('settings_api_key').value = results.key;
+    } else {
+        // version も登録され、profiles も保存済み -> 通常起動
+        console.log('Misskey Now: Restore Settings.');
+    }
+    saveSettings = results.profiles;
+    console.log('Misskey Now (Debug):' + results);
 });
 
-function getUrl(){
-    chrome.tabs.query({ active: true, currentWindow: true },
-        function (tabs) {
-            const url = tabs[0].url;
-            const title = tabs[0].title;
-            document.getElementById("popup_title").value = title;
-            document.getElementById("popup_url").value = url;
-        })
+function getUrl() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const url = tabs[0].url;
+        const title = tabs[0].title;
+        document.getElementById('popup_title').value = title;
+        document.getElementById('popup_url').value = url;
+    });
 }
 
 getUrl();
 
-function buttonstatus(mode){ // default, success, abort
-    if(mode == "default"){
-        document.querySelector(".btn-send").className = "btn-sm w-100 btn-send btn-primary";
-        document.querySelector(".btn-send").textContent = "Misskey Now!";
-    } else if (mode == "success") {
-        document.querySelector(".btn-send").className = "btn-sm w-100 btn-send btn-success";
-        document.querySelector(".btn-send").textContent = "Success";  
-    } else if (mode == "abort") {
-        document.querySelector(".btn-send").className = "btn-sm w-100 btn-send btn-danger";
-        document.querySelector(".btn-send").textContent = "Error";
+function buttonstatus(mode) {
+    // default, success, abort
+    if (mode == 'default') {
+        document.querySelector('.btn-send').className =
+            'btn-sm w-100 btn-send btn-primary';
+        document.querySelector('.btn-send').textContent = 'Misskey Now!';
+    } else if (mode == 'success') {
+        document.querySelector('.btn-send').className =
+            'btn-sm w-100 btn-send btn-success';
+        document.querySelector('.btn-send').textContent = 'Success';
+    } else if (mode == 'abort') {
+        document.querySelector('.btn-send').className =
+            'btn-sm w-100 btn-send btn-danger';
+        document.querySelector('.btn-send').textContent = 'Error';
     }
 }
 
 function generateNote() {
-    const title = document.getElementById("popup_title").value
-    const url =  document.getElementById("popup_url").value
-    const range = document.getElementById("popup_range").value;
-    const hash = document.getElementById("popup_hash").value;
-    const note = document.getElementById("popup_note").value;
-    const host = "https://" + document.getElementById("settings_host").value + "/api/notes/create";
-    const apiKey = document.getElementById("settings_api_key").value;
-    strings = note + "\n\n" + "『" + title + "』 - " + url + " " + hash;
+    const title = document.getElementById('popup_title').value;
+    const url = document.getElementById('popup_url').value;
+    const range = document.getElementById('popup_range').value;
+    const hash = document.getElementById('popup_hash').value;
+    const note = document.getElementById('popup_note').value;
+    const host =
+        'https://' +
+        document.getElementById('settings_host').value +
+        '/api/notes/create';
+    const apiKey = document.getElementById('settings_api_key').value;
+    strings = note + '\n\n' + '『' + title + '』 - ' + url + ' ' + hash;
     console.log(strings);
-    let data =
-    {
-        "i": apiKey,
-        "visibility": range,
-        "text": strings
+    let data = {
+        i: apiKey,
+        visibility: range,
+        text: strings,
     };
     str = JSON.stringify(data);
-    console.log(str)
+    console.log(str);
 
-        fetch(host, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: str
-        }).then(response =>{
+    fetch(host, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: str,
+    })
+        .then((response) => {
             if (!response.ok) {
                 console.error('Misskey-now: Response Error!');
-                buttonstatus("abort")
+                buttonstatus('abort');
             } else {
                 console.error('Misskey-now: Send');
-                buttonstatus("success");
+                buttonstatus('success');
             }
-            response.text()
+            response.text();
         })
-        .then(data => {
-                console.log(data);
+        .then((data) => {
+            console.log(data);
         })
         .catch((error) => {
             console.error('Misskey-now: Internal Error! : ' + error);
-            buttonstatus("abort");
-    });
+            buttonstatus('abort');
+        });
 }
 
 function saveSetting(select) {
     settings = {
-        profile: document.getElementById("settings_profile_name").value,
-        instance: document.getElementById("settings_host").value,
-        key: document.getElementById("settings_api_key").value
+        profile: document.getElementById('settings_profile_name').value,
+        instance: document.getElementById('settings_host').value,
+        key: document.getElementById('settings_api_key').value,
     };
 
-    if (select != 'new'){
-        chrome.storage.local.set(settings, function() {
+    if (select != 'new') {
+        chrome.storage.local.set(settings, function () {
             console.log('Misskey-Now: Stored Settings.');
         });
     } else {
-        
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector(".btn-send").addEventListener("click", generateNote);
-    document.querySelector(".btn-save").addEventListener("click", saveSetting);
+    document.querySelector('.btn-send').addEventListener('click', generateNote);
+    document.querySelector('.btn-save').addEventListener('click', saveSetting);
 });
