77f6792f8aac91218dfdcc99dfe74fd610791663
sasakulari <sasakura.ayato.syosetu@gmail.com>
2023-02-11 09:19:42 +0900

    Lint: with Prettier

--- main.js
+++ main.js
@@ -1,90 +1,99 @@
-chrome.storage.local.get(["instance", "key"]).then((results) => {
-    if (typeof results.instance !== 'undefined'){
-        document.getElementById("settings_host").value = results.instance;
-        document.getElementById("settings_api_key").value = results.key;
-    };
+chrome.storage.local.get(['instance', 'key']).then((results) => {
+    if (typeof results.instance !== 'undefined') {
+        document.getElementById('settings_host').value = results.instance;
+        document.getElementById('settings_api_key').value = results.key;
+    }
 });
 
-function getUrl(){
-    chrome.tabs.query({ active: true, currentWindow: true },
-        function (tabs) {
-            const url = tabs[0].url;
-            const title = tabs[0].title;
-            document.getElementById("popup_title").value = title;
-            document.getElementById("popup_url").value = url;
-        })
+function getUrl() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const url = tabs[0].url;
+        const title = tabs[0].title;
+        document.getElementById('popup_title').value = title;
+        document.getElementById('popup_url').value = url;
+    });
 }
 
 getUrl();
 
-function buttonstatus(mode){ // default, success, abort
-    if(mode == "default"){
-        document.querySelector(".btn-send").className = "btn-sm w-100 btn-send btn-primary";
-        document.querySelector(".btn-send").textContent = "Misskey Now!";
-    } else if (mode == "success") {
-        document.querySelector(".btn-send").className = "btn-sm w-100 btn-send btn-success";
-        document.querySelector(".btn-send").textContent = "Success";  
-    } else if (mode == "abort") {
-        document.querySelector(".btn-send").className = "btn-sm w-100 btn-send btn-danger";
-        document.querySelector(".btn-send").textContent = "Error";
+function buttonstatus(mode) {
+    // default, success, abort
+    if (mode == 'default') {
+        document.querySelector('.btn-send').className =
+            'btn-sm w-100 btn-send btn-primary';
+        document.querySelector('.btn-send').textContent = 'Misskey Now!';
+    } else if (mode == 'success') {
+        document.querySelector('.btn-send').className =
+            'btn-sm w-100 btn-send btn-success';
+        document.querySelector('.btn-send').textContent = 'Success';
+    } else if (mode == 'abort') {
+        document.querySelector('.btn-send').className =
+            'btn-sm w-100 btn-send btn-danger';
+        document.querySelector('.btn-send').textContent = 'Error';
     }
 }
 
 function generateNote() {
-    const title = document.getElementById("popup_title").value
-    const url =  document.getElementById("popup_url").value
-    const range = document.getElementById("popup_range").value;
-    const hash = document.getElementById("popup_hash").value;
-    const note = document.getElementById("popup_note").value;
-    const host = "https://" + document.getElementById("settings_host").value + "/api/notes/create";
-    const apiKey = document.getElementById("settings_api_key").value;
-    strings = note + "\n\n" + "『" + title + "』 - " + url + " " + hash;
+    const title = document.getElementById('popup_title').value;
+    const url = document.getElementById('popup_url').value;
+    const range = document.getElementById('popup_range').value;
+    const hash = document.getElementById('popup_hash').value;
+    const note = document.getElementById('popup_note').value;
+    const host =
+        'https://' +
+        document.getElementById('settings_host').value +
+        '/api/notes/create';
+    const apiKey = document.getElementById('settings_api_key').value;
+    strings = note + '\n\n' + '『' + title + '』 - ' + url + ' ' + hash;
     console.log(strings);
-    let data =
-    {
-        "i": apiKey,
-        "visibility": range,
-        "text": strings
+    let data = {
+        i: apiKey,
+        visibility: range,
+        text: strings,
     };
     str = JSON.stringify(data);
-    console.log(str)
+    console.log(str);
 
-        fetch(host, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: str
-        }).then(response =>{
+    fetch(host, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: str,
+    })
+        .then((response) => {
             if (!response.ok) {
                 console.error('Misskey-now: Response Error!');
-                buttonstatus("abort")
+                buttonstatus('abort');
             } else {
                 console.error('Misskey-now: Send');
-                buttonstatus("success");
+                buttonstatus('success');
             }
-            response.text()
+            response.text();
         })
-        .then(data => {
-                console.log(data);
+        .then((data) => {
+            console.log(data);
         })
         .catch((error) => {
             console.error('Misskey-now: Internal Error! : ' + error);
-            buttonstatus("abort");
-    });
+            buttonstatus('abort');
+        });
 }
 
 function saveSetting() {
     settings = {
-        instance: document.getElementById("settings_host").value,
-        key: document.getElementById("settings_api_key").value
+        instance: document.getElementById('settings_host').value,
+        key: document.getElementById('settings_api_key').value,
     };
-    chrome.storage.local.set(settings, function() {
+    chrome.storage.local.set(settings, function () {
         console.log('Misskey-Now: Stored Settings.');
         const prevText = save_settings.textContent;
         const prevClass = save_settings.className;
         save_settings.textContent = '✓';
-        save_settings.className = prevClass.replace('btn-primary', 'btn-success');
+        save_settings.className = prevClass.replace(
+            'btn-primary',
+            'btn-success'
+        );
         setTimeout(() => {
             save_settings.textContent = prevText;
             save_settings.className = prevClass;
@@ -94,11 +103,11 @@ function saveSetting() {
 
 function handleCtrlEnter(e) {
     if (e.ctrlKey && e.code === 'Enter')
-        document.querySelector(".btn-send")?.click();
+        document.querySelector('.btn-send')?.click();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector(".btn-send").addEventListener("click", generateNote);
-    document.querySelector(".btn-save").addEventListener("click", saveSetting);
+    document.querySelector('.btn-send').addEventListener('click', generateNote);
+    document.querySelector('.btn-save').addEventListener('click', saveSetting);
     popup_note.addEventListener('keydown', handleCtrlEnter);
 });
